fc737313edc5eb0f406535cc1553c88dc9fbd227
ITDoctor40148 <zeus40148@hotmail.com>
2020-11-24 22:04:37 +0800

    first

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,107 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" v-if="results">
+    <div class="card-header-custom mx-5">
+      <div>
+        <p class="title mt-5">Overall Rating</p>
+        <div class="votes">
+          <p class="mb-0 mr-2 marks text-green">4.2</p>
+          <StarRating v-model="rating"></StarRating>
+          <p class="ml-3 mb-0">{{ results.dob.age }} Votes</p>
+        </div>
+      </div>
+      <button class="btn-add">+</button>
+    </div>
+    <hr class="my-4" />
+    <div class="user-container mx-5">
+      <div class="user">
+        <img
+          class="avatar"
+          :src="results.picture.thumbnail"
+          alt="avatar"
+        />
+        <div class="ml-3">
+          <p class="title mb-0">{{ `${results.name.first} ${results.name.last}` }}</p>
+          <div class="votes">
+            <StarRating v-model="rating"></StarRating>
+            <p class="ml-3 mb-0">{{ `6` }} ago</p>
+          </div>
+        </div>
+      </div>
+      <p class="mb-0 mt-3 font-13">
+        This place is all you need for the short or long trip to Tbilisi.
+        Perfect location! Sparkling clean and mordern.
+      </p>
+      <p class="mb-0 mt-3">from Facebook</p>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
+import StarRating from "./components/StarRating";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    StarRating,
+  },
+  data() {
+    return {
+      results: null,
+      rating: 3,
+    };
+  },
+  created() {
+    axios.get("https://randomuser.me/api").then((response) => {
+      this.results = response.data.results[0];
+      console.log(this.results)
+    });
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  border-radius: 15px;
+  width: 400px;
+  height: 400px;
+}
+.card-header-custom {
+  display: flex;
+  justify-content: space-between;
+}
+.title {
+  text-align: left;
+  font-size: 14pt;
+  font-weight: 600;
+}
+.btn-add {
+  width: 60px;
+  height: 60px;
+  margin-top: auto;
+  margin-bottom: auto;
+  background-color: rgb(243, 246, 246);
+  border: none;
+  border-radius: 5px;
+  outline: none;
+}
+.font-13 {
+  font-size: 13pt;
+}
+.marks {
+  font-size: 14pt;
+}
+.votes {
+  display: flex;
+}
+.user {
+  display: flex;
+}
+.user .avatar {
+  width: 60px;
+  height: 60px;
+  margin-top: auto;
+  margin-bottom: auto;
+  border-radius: 15px;
 }
 </style>
---
+++ src/dist/main.dev.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var _vue = _interopRequireDefault(require("vue"));
+
+var _App = _interopRequireDefault(require("./App.vue"));
+
+require("bootstrap/dist/css/bootstrap.css");
+
+require("bootstrap-vue/dist/bootstrap-vue.css");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+_vue["default"].config.productionTip = false;
+new _vue["default"]({
+  render: function render(h) {
+    return h(_App["default"]);
+  }
+}).$mount('#app');
